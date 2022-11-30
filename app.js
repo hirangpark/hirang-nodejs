43de3ee8a3e259ec4c5bef539735f313c5a3bf90
AD\hirang <semp0110@naver.com>
2022-11-30 11:26:05 +0900

    소스 수정

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // 앱
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World 안녕하세요????ecr');
+  res.send('Hello World');
 });
 
 app.listen(PORT, HOST, () => {
